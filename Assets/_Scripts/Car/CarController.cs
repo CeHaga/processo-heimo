300cbb02b23e2c5c4a2818a459b5857d565fc175
Carlos Bravo <carlosh.serrado@gmail.com>
2024-01-24 19:14:44 -0300

    Exclui assets não utilizados

---
+++ Assets/_Scripts/Car/CarController.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentbreakForce;
+    private bool isBreaking;
+
+    [Header("Car Settings")]
+    [SerializeField] private float motorForce, breakForce, maxSteerAngle;
+
+    [Header("Wheel Colliders")]
+    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+
+    [Header("Wheel Transforms")]
+    [SerializeField] private Transform[] wheelsGroup;
+    private Transform frontLeftWheelTransform, frontRightWheelTransform;
+    private Transform rearLeftWheelTransform, rearRightWheelTransform;
+
+    [Header("Events")]
+    public UnityEvent onCollectibleCollected;
+
+    [Header("SFX")]
+    [SerializeField] private AudioClip honk;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = honk;
+    }
+
+    private void Start()
+    {
+        foreach (Transform wheelGroup in wheelsGroup)
+        {
+            if (!wheelGroup.gameObject.activeSelf) continue;
+            foreach (Transform wheel in wheelGroup)
+            {
+                if (wheel.CompareTag("FLWheel")) frontLeftWheelTransform = wheel;
+                if (wheel.CompareTag("FRWheel")) frontRightWheelTransform = wheel;
+                if (wheel.CompareTag("BLWheel")) rearLeftWheelTransform = wheel;
+                if (wheel.CompareTag("BRWheel")) rearRightWheelTransform = wheel;
+            }
+            break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        // Workshop Input
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Workshop");
+        }
+
+        // Steering Input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Acceleration Input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Breaking Input
+        isBreaking = Input.GetKey(KeyCode.Space);
+
+        // Honk Input
+        if (Input.GetKeyDown(KeyCode.Q) && audioSource)
+        {
+            audioSource.Play();
+        }
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        currentbreakForce = isBreaking ? breakForce : 0f;
+        ApplyBreaking();
+    }
+
+    private void ApplyBreaking()
+    {
+        frontRightWheelCollider.brakeTorque = currentbreakForce;
+        frontLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearLeftWheelCollider.brakeTorque = currentbreakForce;
+        rearRightWheelCollider.brakeTorque = currentbreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            Destroy(other.gameObject);
+            onCollectibleCollected.Invoke();
+        }
+        if (other.CompareTag("Void"))
+        {
+            SceneManager.LoadScene("Workshop");
+        }
+    }
+
+    private void CheckIfCarIsUpsideDown()
+    {
+        // Check if car is upside down for 3 seconds
+        if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 270)
+        {
+            Debug.Log("Upside down");
+            StartCoroutine(ResetCar());
+        }
+    }
+
+    private IEnumerator ResetCar()
+    {
+        yield return new WaitForSeconds(3f);
+        if (transform.eulerAngles.z <= 90 || transform.eulerAngles.z >= 270) yield break;
+        SceneManager.LoadScene("Workshop");
+    }
+}
